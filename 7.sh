daea7710ae2a5c84d94434010d05da3ca439451e
Gokul Adethya <frozenwolf2003.com@gmail.com>
2025-04-20 04:29:54 +0530

    lwf, ewc

---
+++ 7.sh
@@ -0,0 +1,1 @@
+torchrun   --nnodes=1   --nproc_per_node=1   --rdzv_id=$RANDOM   --rdzv_backend=c10d   --rdzv_endpoint=$head_node_ip:29500   /scratch/gokuladethya.cse.nitt/Indic-CL-ASR/cl_baseline_lwf.py   --notes "CL-baseline-lwf-CTX_1_KD_0.1" --cl_config.knowledge_distillation_ctx 1
